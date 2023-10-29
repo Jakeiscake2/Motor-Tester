816efcb25c258738d0f2cdee3cdb0154caef93a9
Yulian-Zhu-yu <117557803+Yulian-Zhu-yu@users.noreply.github.com>
2023-10-28 22:59:20 -0600

    quick save, code doesnt work btw

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,16 @@
 using namespace pros;
 
 Controller controls(E_CONTROLLER_MASTER);
-Motor motor1(1, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
-Motor motor2(2, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
-Motor motor3(3, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
-Motor motor4(4, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
-Motor motor5(5, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
-Motor motor6(6, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
-Motor motor7(7, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
-Motor motor8(8, E_MOTOR_GEARSET_36, false, E_MOTOR_ENCODER_DEGREES);
+Motor motor2(2, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+Motor motor3(3, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+Motor motor4(4, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+Motor motor5(5, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+Motor motor6(6, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+Motor motor7(7, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
+Motor motor8(8, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
 Motor motorArray[8] = { motor1, motor2, motor3, motor4, motor5, motor6, motor7, motor8 };
 
-int motorControlXPoints[5] = { 2,120,240,360,480 };
+int motorControlXPoints[5] = { 0,120,240,360,480 };
 int motorControlYPoints[3] = { 0,120,240 };
 
 bool intBetween(int range1, int checkNum, int range2) {
@@ -27,8 +26,11 @@ void printText(std::string displayTextString, int xPos, int yPos) {
 	pros::c::screen_print_at(TEXT_SMALL, xPos, yPos, displayTextArray);
 }
 
+//bool yeah[8] = { false,false,false,false,false,false, false, false };
+int motorSpeedArray[8] = { 20,30,40,50,60,70,80,90 };
 struct MotorControl {
-	int motorNum, motorSpeed = 0;
+	int motorNum;
+	Motor motor1(motorNum, E_MOTOR_GEARSET_06, false, E_MOTOR_ENCODER_DEGREES);
 	int x1, x2, y1, y2;
 	int YHeight;
 	int xButtonPoints[4];
@@ -55,7 +57,7 @@ struct MotorControl {
 		xButtonPoints[1] = x1 + ((x2 - x1) / 3);
 		xButtonPoints[2] = x1 + (2 * ((x2 - x1) / 3));
 		xButtonPoints[3] = x1 + (3 * ((x2 - x1) / 3));
-		YHeight = y1 + (2 * (y2 - y1) / 3);
+		YHeight = y1 + (3 * (y2 - y1) / 4);
 		motorNum = tempMotorNum;
 	}
 	void updateScreen() {
@@ -63,39 +65,44 @@ struct MotorControl {
 		screen::set_pen(COLOR_WHITE);
 		screen::draw_rect(x1 + 2, y1, x2 - 2, YHeight - 2);
 
-		printText("M" + std::to_string(motorNum) + ": " + std::to_string(round(motorArray[motorNum - 1].get_position())), x1 + 4, y1 + 4);
-		printText("V: " + std::to_string(round(motorArray[motorNum - 1].get_actual_velocity()) + 0), x1 + 4, y1 + 20);
-		printText("W: " + std::to_string(round(motorArray[motorNum - 1].get_current_draw() * 0.001)), x1 + 4, y1 + 36);
-		printText("T: " + std::to_string(round(motorArray[motorNum - 1].get_temperature())), x1 + 4, y1 + 52);
+		printText("M" + std::to_string(motorNum) + ": " + std::to_string(int(motorArray[motorNum - 1].get_position())), x1 + 4, y1 + 4);
+		printText("V: " + std::to_string(int(motorArray[motorNum - 1].get_actual_velocity())), x1 + 4, y1 + 20);
+		printText("EV: " + std::to_string((motorSpeedArray[motorNum - 1] / 127) * 600), x1 + 4, y1 + 36);
+		printText("W: " + std::to_string(float(motorArray[motorNum - 1].get_current_draw()) * 0.001), x1 + 4, y1 + 52);
+		printText("T: " + std::to_string(int(motorArray[motorNum - 1].get_temperature())), x1 + 4, y1 + 69);
 
 		screen::set_pen(COLOR_WHITE);
-		screen::draw_rect(xButtonPoints[0], YHeight, xButtonPoints[1] - 2, y2 - 2);
-		screen::draw_rect(xButtonPoints[1], YHeight, xButtonPoints[2] - 2, y2 - 2);
-		screen::draw_rect(xButtonPoints[2], YHeight, xButtonPoints[3] - 2, y2 - 2);
+		screen::draw_rect(xButtonPoints[0] + 2, YHeight, xButtonPoints[1] - 2, y2 - 2);
+		screen::draw_rect(xButtonPoints[1] + 2, YHeight, xButtonPoints[2] - 2, y2 - 2);
+		screen::draw_rect(xButtonPoints[2] + 2, YHeight, xButtonPoints[3] - 2, y2 - 2);
 
 		screen::set_pen(COLOR_GREEN);
-		pros::c::screen_print_at(TEXT_MEDIUM, xButtonPoints[0] + 3, YHeight + 3, "ON");
+		pros::c::screen_print_at(TEXT_SMALL, xButtonPoints[0] + 5, YHeight + 3, "ON");
 		screen::set_pen(COLOR_RED);
-		pros::c::screen_print_at(TEXT_MEDIUM, xButtonPoints[1] + 3, YHeight + 3, "OFF");
+		pros::c::screen_print_at(TEXT_SMALL, xButtonPoints[1] + 5, YHeight + 3, "OFF");
 		screen::set_pen(COLOR_BLUE);
-		pros::c::screen_print_at(TEXT_MEDIUM, xButtonPoints[2] + 3, YHeight + 3, "REV");
+		pros::c::screen_print_at(TEXT_SMALL, xButtonPoints[2] + 5, YHeight + 3, "REV");
 
+		screen::set_pen(COLOR_WHITE);
 	}
 	int checkButtons(screen_touch_status_s_t status) {
 		int statx = status.x;
 		int staty = status.y;
-		if (intBetween(y1, staty, y2)) {
-			if (intBetween(xButtonPoints[0], statx, xButtonPoints[1])) {
-				motorSpeed = 127;
+		if (intBetween(YHeight, staty, y2 - 2)) {
+			if (intBetween(xButtonPoints[0] + 2, statx, xButtonPoints[1] - 2)) {
+				motorSpeedArray[motorNum - 1] = 127;
+				//yeah[motorNum - 1] = true;
 			}
-			else if (intBetween(xButtonPoints[1], statx, xButtonPoints[2])) {
-				motorSpeed = 0;
+			else if (intBetween(xButtonPoints[1] + 2, statx, xButtonPoints[2] - 2)) {
+				motorSpeedArray[motorNum - 1] = 0;
+				//yeah[motorNum - 1] = false;
 			}
-			else if (intBetween(xButtonPoints[2], statx, xButtonPoints[3])) {
-				motorSpeed = -127;
+			else if (intBetween(xButtonPoints[2] + 2, statx, xButtonPoints[3] - 2)) {
+				motorSpeedArray[motorNum - 1] = -127;
+				//yeah[motorNum - 1] = true;
 			}
 		}
-		return motorSpeed;
+		return motorSpeedArray[motorNum - 1];
 	}
 };
 
@@ -112,20 +119,32 @@ void opcontrol() {
 	MotorControl motorControl6(6);
 	MotorControl motorControl7(7);
 	MotorControl motorControl8(8);
-	MotorControl* motorControlArray[8] = { &motorControl1, &motorControl2, &motorControl3, &motorControl4 , &motorControl5, &motorControl6, &motorControl7, &motorControl8 };
+	MotorControl* motorControlArray[8] = { &motorControl1, &motorControl2, &motorControl3, &motorControl4, &motorControl5, &motorControl6, &motorControl7, &motorControl8 };
+	//MotorControl* motorControlArray[8] = { &motorControl1, &motorControl2, &motorControl3, &motorControl4, &motorControl5, &motorControl6, &motorControl7, &motorControl8 };
 
 	screen::set_eraser(COLOR_BLACK);
+	screen_touch_status_s_t prevTouchStatus;
 	while (true) {
 		screen::erase();
-		screen_touch_status_s_t touchStatus;
+		//printText("yeah cool " + std::to_string(touchStatus.x) + ", " + std::to_string(touchStatus.y), 300, 0);
 		for (int it = 0;it < 8;it++) {
-			MotorControl curMotorControl = *motorControlArray[it];
-			curMotorControl.updateScreen();
-			if (intBetween(0, motorArray[curMotorControl.motorNum - 1].get_position(), 600)) {
-				touchStatus = c::screen_touch_status();
-				motorArray[curMotorControl.motorNum - 1] = curMotorControl.checkButtons(touchStatus);
-			}
+			(*motorControlArray[it]).updateScreen();
+			/*
+			if (yeah[it]) {
+				screen::draw_rect(100, 100, 200, 100 * curMotorControl.motorNum);
+				pros::c::screen_print_at(TEXT_MEDIUM, 50 * curMotorControl.motorNum, 200, "YEAHY!!!!!!");
+			}*/
 		}
-		delay(80);
+		screen_touch_status_s_t curTouchStatus;
+		prevTouchStatus = c::screen_touch_status();
+		if (curTouchStatus.x != prevTouchStatus.x && curTouchStatus.y != prevTouchStatus.y) {
+			for (int it = 0;it < 8;it++) {
+				if (intBetween(-10, motorArray[it].get_position(), 600)) {
+					curTouchStatus = c::screen_touch_status();
+					motorArray[it] = (*motorControlArray[it]).checkButtons(curTouchStatus);
+				}
+			}
+			delay(60);
+		}
 	}
 }
